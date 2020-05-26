c08a11bbbfb287637e458e9d046d977cd1d84b6c
alectrocute <35377827+alectrocute@users.noreply.github.com>
2020-05-25 18:44:09 -0700

    fix localstorage

--- src/store/settingsLoader.js
+++ src/store/settingsLoader.js
@@ -1,34 +1,43 @@
-const fs = require('fs')
-const path = require('path')
+const fs = require("fs");
+const path = require("path");
+const electron = require("electron");
+const appPath = electron.remote.app.getPath("userData");
 
 const load = () => {
-    let jsonSettings;
-    try {
-        let f = fs.readFileSync(path.join(__static, '/settings.json'))
-        jsonSettings = JSON.parse(f)
-        console.log("Loaded configuration file!")
-        console.log(jsonSettings)
-    } catch (e) {
-        window.console.log(`Generating settings config file... ${path.join(__static, '/settings.json')}`)
-        fs.writeFileSync(path
-            .join(__static, '/settings.json'),
-            JSON.stringify(require('./config.default.js').ob)
-        );
-    }
-    return jsonSettings
-}
+  let jsonSettings;
+  try {
+    let f = fs.readFileSync(path.join(appPath, "/settings.json"));
+    jsonSettings = JSON.parse(f);
+    console.log("Loaded configuration file!", appPath + "/settings.json");
+    console.log(jsonSettings);
+  } catch (e) {
+    window.console.log(
+      `Generating settings config file... ${path.join(
+        appPath,
+        "/settings.json"
+      )}`
+    );
+    fs.writeFileSync(
+      path.join(appPath, "/settings.json"),
+      JSON.stringify(require("./config.default.js").ob)
+    );
+  }
+  return jsonSettings;
+};
 
-const save = (s) => {
-    try {
-        fs.writeFileSync(path.join(__static, '/settings.json'), JSON.stringify(s));
-    } catch (e) {
-        window.alert(e)
-        window.console.log(`Couldn't write to config file.. ${path.join(__static, '/settings.json')}`)
-    }
-    return
-}
+const save = s => {
+  try {
+    fs.writeFileSync(path.join(appPath, "/settings.json"), JSON.stringify(s));
+  } catch (e) {
+    window.alert(e);
+    window.console.log(
+      `Couldn't write to config file.. ${path.join(appPath, "/settings.json")}`
+    );
+  }
+  return;
+};
 
 module.exports = {
-    load,
-    save
-}
+  load,
+  save
+};
